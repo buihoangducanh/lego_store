83287fb4c5c127d58217776ed2746edde618a5a3
Anh bui <buihoangducanh0987@gmail.com>
2023-06-12 07:35:33 +0700

    add search bar

--- productList.php
+++ productList.php
@@ -5,7 +5,6 @@ session_start();
 <?php
 include 'util/connectDB.php';
 
-
 // Xử lý trang hiện tại và số sản phẩm trên mỗi trang
 $perPage = 8;
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
@@ -18,12 +17,24 @@ $categoriesList = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 // Lấy danh sách sản phẩm theo danh mục và phân trang
 $cateId = isset($_GET['cateId']) ? $_GET['cateId'] : 2;
-$sql = "SELECT * FROM products WHERE cateId = '$cateId' AND status = 1 LIMIT $offset, $perPage";
+
+// Xử lý tìm kiếm
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+if (!empty($search)) {
+    $search = mysqli_real_escape_string($conn, $search);
+    $sql = "SELECT * FROM products WHERE cateId = '$cateId' AND status = 1 AND name LIKE '%$search%' LIMIT $offset, $perPage";
+} else {
+    $sql = "SELECT * FROM products WHERE cateId = '$cateId' AND status = 1 LIMIT $offset, $perPage";
+}
 $result = mysqli_query($conn, $sql);
 $list = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 // Đếm số lượng sản phẩm để tính toán số trang
-$sqlCount = "SELECT COUNT(*) AS total FROM products WHERE cateId = '$cateId' AND status = 1";
+if (!empty($search)) {
+    $sqlCount = "SELECT COUNT(*) AS total FROM products WHERE cateId = '$cateId' AND status = 1 AND name LIKE '%$search%'";
+} else {
+    $sqlCount = "SELECT COUNT(*) AS total FROM products WHERE cateId = '$cateId' AND status = 1";
+}
 $resultCount = mysqli_query($conn, $sqlCount);
 $rowCount = mysqli_fetch_assoc($resultCount);
 $totalProducts = $rowCount['total'];
@@ -46,9 +57,9 @@ mysqli_close($conn);
     <?php include 'inc/header.php' ?>
     <section class="banner"></section>
     <div class="container-intro-section black-background">
-        <h1 class="text-center text-light">Danh sách sản phẩm</h1>
+        <h1 class="text-center text-light ">Danh sách sản phẩm</h1>
 
-        <div class="category" style="margin-left: 200px;">
+        <div class="category " style="margin-left: 200px;">
             <span class="text-light" style="margin-bottom: 20px; display: inline-block;">Danh mục:</span>
             <select onchange="location = this.value;">
                 <?php
@@ -61,7 +72,16 @@ mysqli_close($conn);
                 }
                 ?>
             </select>
+
         </div>
+        <div class="search w-50 " style="margin-left: 200px;">
+            <form class="" action="" method="GET">
+                <input type="hidden" name="cateId" value="<?= $cateId ?>">
+                <input type="text" name="search" placeholder="Tìm kiếm sản phẩm" value="<?= isset($_GET['search']) ? $_GET['search'] : '' ?>">
+                <button class="btn btn-dark p-2 mb-4" type="submit">Tìm kiếm</button>
+            </form>
+        </div>
+
     </div>
 
     <div class="container">
@@ -102,21 +122,22 @@ mysqli_close($conn);
             <?php }
         } else { ?>
             <h3>Không có sản phẩm nào...</h3>
-        <?php  }
-        ?>
+        <?php } ?>
     </div>
     <div class="pagination">
-        <a href="productList.php?page=<?= ($page <= 1) ? 1 : $page - 1 ?>&cateId=<?= $cateId ?>">&laquo;</a>
-        <?php
-        for ($i = 1; $i <= $pageCount; $i++) {
-            if ($i == $page) {
-                echo '<a class="active" href="productList.php?page=' . $i . '&cateId=' . $cateId . '">' . $i . '</a>';
-            } else {
-                echo '<a href="productList.php?page=' . $i . '&cateId=' . $cateId . '">' . $i . '</a>';
-            }
-        }
-        ?>
-        <a href="productList.php?page=<?= $page + 1 ?>&cateId=<?= $cateId ?>">&raquo;</a>
+        <?php if ($page > 1) { ?>
+            <a href="productList.php?page=<?= ($page <= 1) ? 1 : $page - 1 ?>&cateId=<?= $cateId ?>&search=<?= isset($_GET['search']) ? $_GET['search'] : '' ?>">&laquo;</a>
+        <?php } ?>
+        <?php for ($i = 1; $i <= $pageCount; $i++) {
+            if ($i == $page) { ?>
+                <a class="active" href="productList.php?page=<?= $i ?>&cateId=<?= $cateId ?>&search=<?= isset($_GET['search']) ? $_GET['search'] : '' ?>"><?= $i ?></a>
+            <?php } else { ?>
+                <a href="productList.php?page=<?= $i ?>&cateId=<?= $cateId ?>&search=<?= isset($_GET['search']) ? $_GET['search'] : '' ?>"><?= $i ?></a>
+        <?php }
+        } ?>
+        <?php if ($page < $pageCount) { ?>
+            <a href="productList.php?page=<?= $page + 1 ?>&cateId=<?= $cateId ?>&search=<?= isset($_GET['search']) ? $_GET['search'] : '' ?>">&raquo;</a>
+        <?php } ?>
     </div>
 </body>
 
