0cff5859e840a2613059fef311783dcc59e73e0a
Anh bui <buihoangducanh0987@gmail.com>
2023-06-17 15:59:36 +0700

    add 404.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <footer style="margin-top: 100px;">
     <div class="social">
-        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="https://www.facebook.com/ducanh.buihoang/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
         <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
         <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
     </div>
